6c2f3f676f0eba5488557292681eeaad45409381
ndurushima <ndu1994@gmail.com>
2025-08-19 11:27:54 -0500

    displaying each table separately

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ import sqlite3
 import pandas as pd
 conn = sqlite3.connect('data.sqlite')
 
-
+# select all records from orderdetails and products and join them using their common key productCode and display the first 10.
 q = """
 SELECT *
  FROM orderdetails
       JOIN products
       ON orderdetails.productCode = products.productCode
       LIMIT 10;
+"""
+print(pd.read_sql(q, conn))
+
+
+# select all records from orderdetails and display the first 10:
+q = """
+SELECT *
+ FROM orderdetails
+ LIMIT 10;
+"""
+print(pd.read_sql(q, conn))
+
+
+# select all records from products and display the first 10:
+q = """
+SELECT *
+ FROM products
+ LIMIT 10;
 """
 print(pd.read_sql(q, conn))
