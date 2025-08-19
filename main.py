38a9bcea3e65272b466de5c662784f1d5b3aea37
ndurushima <ndu1994@gmail.com>
2025-08-19 11:24:38 -0500

    Select ALL records from orderDetails and products and Join them using their common key ProductCode

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import sqlite3
 import pandas as pd
 conn = sqlite3.connect('data.sqlite')
+
+
+q = """
+SELECT *
+ FROM orderdetails
+      JOIN products
+      ON orderdetails.productCode = products.productCode
+      LIMIT 10;
+"""
+print(pd.read_sql(q, conn))
